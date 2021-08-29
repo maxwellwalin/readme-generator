2aca8281eafcdc1143f61f29482980e83edccb46
maxwell walin <maxwellwalin@gmail.com>
2021-08-29 11:33:00 -0700

    created start files, including basic starter code in index and installed json packages

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+const questions = ["Enter the project title: ", "Enter a description of your project: ", "Enter instructions to install your project: ", "Enter your project's usage information: ", "Enter the guidelines for other developer's contributions: ", "Enter the test instructions for your project:"];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md", 
+    
+    )
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+}
+
+// Function call to initialize app
+init();
